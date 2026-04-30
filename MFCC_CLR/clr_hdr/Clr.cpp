ad118a2c1f7c4f53cc9756d93826f6df48c85553
Vivek95vs <55971500+Vivek95vs@users.noreply.github.com>
2026-04-30 17:51:30 +0530

    Add files via upload

---
+++ MFCC_CLR/clr_hdr/Clr.cpp
@@ -0,0 +1,35 @@
+#include "Clr.h"
+#include <opencv2/opencv.hpp>
+#include<iostream>
+#include <experimental/filesystem>
+#include "D:\\chaitanya\\HDR scaling image\\HDR with sql\\HDR_MAIN_CODE\\HDR\\HDR\\HDR.cpp"
+#include "D:\\chaitanya\\HDR scaling image\\HDR with sql\\HDR_MAIN_CODE\\HDR\\HDR\\HDR_sql.cpp"
+
+namespace hdr_clr {
+
+
+
+	HDR_CLR::HDR_CLR() {
+		hdr = new HDR();
+	}
+
+	HDR_CLR::~HDR_CLR() {
+	
+	}
+	HDR_CLR::!HDR_CLR() {
+
+	}
+
+	int HDR_CLR::caliblengthandpixels() {
+		cal=hdr->caliblength();
+		return cal;
+	}
+	double HDR_CLR::loadingimages(int value) {
+		leng=hdr->loadimage(value);
+
+		HDR_Path = gcnew System::String(hdr->filePath.c_str());
+
+		return leng;
+	}
+	
+}
---
+++ MFCC_CLR/clr_hdr/Clr.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "D:\\chaitanya\\HDR scaling image\\HDR with sql\\HDR_MAIN_CODE\\HDR\\HDR\\\\HDR.h"
+#include "D:\\chaitanya\\HDR scaling image\\HDR with sql\\HDR_MAIN_CODE\\HDR\\HDR\\HDR_sql.h"
+using namespace System;
+
+
+namespace hdr_clr {
+
+	public ref class HDR_CLR
+	{
+	public:
+
+		HDR_CLR();
+
+		~HDR_CLR();
+		!HDR_CLR();
+
+		int cal; 
+		double leng;
+		System::String^ HDR_Path;
+
+		int caliblengthandpixels();
+		double loadingimages(int value);
+
+    private:
+		HDR * hdr;
+	};
+}
